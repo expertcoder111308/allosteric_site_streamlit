c02d701d6032f42b8dc828a915f73c638e17add1
expertcoder111308 <57080378+expertcoder111308@users.noreply.github.com>
2024-01-20 15:06:44 -0600

    streamlit app

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+from joblib import load
+
+# Imports the functions we coded above
+from header import *
+from userinput import *
+from response import *
+from predictor import *
+
+# Load our DecisionTree model into our web app
+model = load("model.joblib")
+
+create_header()
+input_features = get_user_input()
+clicked = st.button("Classify")
+if clicked:
+  prediction = make_prediction(model, input_features)
+  get_app_response(prediction)
